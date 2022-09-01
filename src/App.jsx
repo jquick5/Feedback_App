234726124b197bb7a6aae981a1fb477954941a99
jquick5 <jay.quick79@gmail.com>
2022-09-01 09:21:13 -0700

    Added context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FeedbackForm from './components/FeedbackForm';
-
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import Header from './components/Header';
-import FeedbackData from './data/FeedbackData';
+import AboutPage from './pages/AboutPage';
+import { FeedbackProvider } from './context/FeedbackContext';
+import AboutIconLink from './components/AboutIconLink';
 
 function App() {
-	const [feedback, setFeedback] = useState(FeedbackData);
-
-	const deleteFeedback = (id) => {
-		if (window.confirm('Are you sure?')) {
-			setFeedback(feedback.filter((i) => i.id !== id));
-		}
-	};
-
-	const addFeedback = (newFeedback) => {
-		newFeedback.id = uuidv4();
-		setFeedback([newFeedback, ...feedback]);
-	};
-
 	return (
-		<>
-			<Header />
+		<FeedbackProvider>
+			<Router>
+				<Header />
 
-			<div className='container'>
-				<FeedbackForm handleAdd={addFeedback} />
-				<FeedbackStats feedback={feedback} />
-				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-			</div>
-		</>
+				<div className='container'>
+					<Routes>
+						<Route
+							exact
+							path='/'
+							element={
+								<>
+									<FeedbackForm />
+									<FeedbackStats />
+									<FeedbackList />
+								</>
+							}
+						></Route>
+						<Route path='/about' element={<AboutPage />} />
+					</Routes>
+					<AboutIconLink />
+				</div>
+			</Router>
+		</FeedbackProvider>
 	);
 }
 
