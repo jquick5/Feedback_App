aebb889d7e5ea5fd8bc8112f1b75b9596b428c9a
jquick5 <jay.quick79@gmail.com>
2022-09-01 10:09:01 -0700

    Added update feedback functionality

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,23 +22,46 @@ export const FeedbackProvider = ({ children }) => {
 		},
 	]);
 
+	const [feedbackEdit, setFeedbackEdit] = useState({
+		item: {},
+		edit: false,
+	});
+
+	// Delete Feedback
 	const deleteFeedback = (id) => {
 		if (window.confirm('Are you sure?')) {
 			setFeedback(feedback.filter((i) => i.id !== id));
 		}
 	};
 
+	// Add Feedback
 	const addFeedback = (newFeedback) => {
 		newFeedback.id = uuidv4();
 		setFeedback([newFeedback, ...feedback]);
+	};
+
+	// Add item to update
+	const editFeedback = (item) => {
+		setFeedbackEdit({
+			item,
+			edit: true,
+		});
+	};
+
+	// Update Feedback Data
+	const updateFeedback = (id, updItem) => {
+		setFeedback(feedback.map((i) => (i.id === id ? { ...i, ...updItem } : i)));
 	};
 
 	return (
 		<FeedbackContext.Provider
 			value={{
 				feedback,
+				feedbackEdit,
 				deleteFeedback,
 				addFeedback,
+				editFeedback,
+				updateFeedback,
 			}}
 		>
 			{children}
